ca477439c017fedb64f4f6311697a36f535e6f1d
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-02-13 12:48:45 -0800

    Added Read Data (CRUD) to DiaryEntriesController.cs to be able to read data and insert it into a table

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,33 @@ namespace DiaryApp.Data
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry
+                {
+                    Id = 1,
+                    Title = "First Entry",
+                    Content = "This is the content of the first entry.",
+                    Created = DateTime.FromOADate(2025 - 02 - 11)
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Second Entry",
+                    Content = "This is the content of the second entry.",
+                    Created = DateTime.FromOADate(2025 - 02 - 12)
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Third Entry",
+                    Content = "This is the content of the third entry.",
+                    Created = DateTime.FromOADate(2025 - 02 - 13)
+                }
+                );
+        }
     }
 }
---
+++ DiaryApp/Migrations/20250213201355_SeedDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "This is the content of the first entry.", new DateTime(1905, 7, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "First Entry" },
+                    { 2, "This is the content of the second entry.", new DateTime(1905, 7, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Second Entry" },
+                    { 3, "This is the content of the third entry.", new DateTime(1905, 7, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Third Entry" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
