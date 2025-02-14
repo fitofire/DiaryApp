fde9f9deceb1a555175d1d3806198c02b550fd6d
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-02-14 06:40:12 -0800

    Added slugs to the Model/DiaryEntry.cs class, Program.cs and Views/DiaryEntries/Index.cshtml file

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -9,7 +9,7 @@ namespace DiaryApp.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please enter a title!")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 30 characters")]
         public string Title { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter what you did today!")]
@@ -17,6 +17,13 @@ namespace DiaryApp.Models
 
         [Required(ErrorMessage = "Please enter a date!")]
         public DateTime Created { get; set; } = DateTime.Now;
+
+        // Add a read-only property named Slug
+        // public string Slug => Title?.Replace(' ', '-').ToLower() + '-' + Created.ToString();
+        public string Slug => (Title?.Length > 7 ? Title.Substring(0, 7) : Title)?.Replace(' ', '-').ToLower()
+                      + "-" + Created.ToString("yyyyMMdd");
+
+
 
     }
 }
